b1b3db4446b1eb067a687add4d42cc07a8e73fb6
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-09-21 11:01:23 -0700

    feat: Add function for getting all workspaces

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,14 @@
 export = workspace;
 /**
  * @param {string} name The workspace name to query for location
- * @return {string | undefined } The directory path to the workspace if it exists
- * */
+ * @return {string | undefined} The directory path to the workspace if it exists
+ **/
 declare function workspace(name: string): string | undefined;
+declare namespace workspace {
+    export { all };
+}
+/**
+ * @return {Map<string, string>} A map of all workspaces to filesystem paths
+ **/
+declare function all(): Map<string, string>;
 //# sourceMappingURL=index.d.ts.map
--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const getCallerPath = require("caller-path");
 const findup = require("findup-sync");
 
 /**
- * @param {string} name The workspace name to query for location
- * @return {string | undefined } The directory path to the workspace if it exists
- * */
-function workspace(name) {
-  const callerPath = getCallerPath();
+ * @param {string | undefined} callerPath The caller path to use as the root
+ * @return {Map<string, string>} A map of workspaces to filesystem paths
+ **/
+function lookup(callerPath) {
   // If we can't get a callerPath, just assume the root is `cwd`
   const searchPath = callerPath ? path.dirname(callerPath) : process.cwd();
   // From the search root, we need to traverse upwards and find the package-lock.json file
@@ -20,7 +19,31 @@ function workspace(name) {
 
   const packages = mapWorkspaces.virtual({ cwd: lockfileRoot, lockfile });
 
+  return packages;
+}
+
+/**
+ * @param {string} name The workspace name to query for location
+ * @return {string | undefined} The directory path to the workspace if it exists
+ **/
+function workspace(name) {
+  const callerPath = getCallerPath();
+
+  const packages = lookup(callerPath);
+
   return packages.get(name);
 }
 
+/**
+ * @return {Map<string, string>} A map of all workspaces to filesystem paths
+ **/
+function all() {
+  const callerPath = getCallerPath();
+
+  const packages = lookup(callerPath);
+
+  return packages;
+}
+
 module.exports = workspace;
+module.exports.all = all;
